436a2c8408fb044013f889fd7ceab2764ba82753
Rúben Freitas <joseruben98@hotmail.com>
2022-06-08 15:55:51 +0100

    missing pt text

--- resources/js/assets/en/activityBiking.js
+++ resources/js/assets/en/activityBiking.js
@@ -2,27 +2,27 @@ export const text = {
     title: "biking",
     subtitle: (<>Meet the <span>island</span> with excitement</>),
     information: [
-        "A Madeira tem alguns dos melhores trilhos do cume ao nível do mar em trilhos rochosos, rodeados por magníficas vistas para o mar.",
-        "Este passeio tem início no terceiro Pico mais alto da ilha (Pico do Areeiro), passando entre trilhos na natureza, desde estradas antigas e levadas, finalizando junto ao mar.",
+        "Madeira has some of the best mountaintop trails at sea level on rocky trails, surrounded by magnificent sea views.",
+        "This tour starts at the third highest peak on the island (Pico do Areeiro), passing through nature trails, from old roads and levadas, ending by the sea.",
     ],
     section: "All necessary information regarding Biking",
     includes: [
-        "Bicicleta com suspensão total e capacete",
-        "Guias especializados e locais",
-        "Transferes e transporte",
-        "Capacete aberto",
-        "Protocolos de segurança e licenças",
-        "Snack e água",
-        "Seguros"
+        "Full suspension bike and helmet",
+        "Expert and local guides",
+        "transfers and transport",
+        "Open helmet",
+        "Security protocols and licenses",
+        "Snack and water",
+        "Insurance"
     ],
     activities: {
-        title: "Informações",
+        title: "Informations",
         items: [
-            "Duração: Dia inteiro",
-            "Nível físico: Baixo/Médio",
-            "Dificuldade técnica: Fácil/Moderada",
-            "Distancia: 45 KM",
-            "Preços especiais para grupos"
+            "Duration: Full day",
+            "Physical level: Low/Medium",
+            "Technical difficulty: Easy/Moderate",
+            "Distance: 45KM",
+            "Special prices for groups"
         ],
     },
     gallery: {
--- resources/js/assets/en/activityCoasteering.js
+++ resources/js/assets/en/activityCoasteering.js
@@ -2,8 +2,8 @@ export const text = {
     title: "coasteering",
     subtitle: (<>Meet the <span>island</span> with excitement</>),
     information: [
-        "O Coasteering na Madeira consiste em explorar a costa marítima através de caminhada, natação, escalada e saltos, com equipamento apropriado e com supervisão dos guias. É uma atividade apropriada para todas as idades, (a partir dos 10 anos) adaptando-se ao perfil de cada pessoa.",
-        "Possibilidade de saltos desde 1 metro até 16 metros, sendo todos estes opcionais.",
+        "Coasteering in Madeira consists of exploring the maritime coast through walking, swimming, climbing and jumping, with appropriate equipment and supervised by guides. It is an activity suitable for all ages (from 10 years old) adapting to the profile of each person.",
+        "Possibility of jumps from 1 meter to 16 meters, all of which are optional.",
     ],
     section: "All necessary information regarding Coasteering",
     includes: [
